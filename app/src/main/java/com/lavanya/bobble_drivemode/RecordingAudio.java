7c698a6a605764bb055eea39f5516fcf4c990cc5
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-01-18 16:46:14 +0530

    xml added

---
+++ app/src/main/java/com/lavanya/bobble_drivemode/RecordingAudio.java
@@ -0,0 +1,13 @@
+package com.lavanya.bobble_drivemode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordingAudio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording_audio);
+    }
+}
